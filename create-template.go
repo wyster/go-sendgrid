a0fcb3572833f74140e14b7ae053f676e8f36477
Ilya Zelenin <wyster@make.im>
2020-10-25 11:08:04 -0700

    Use function

--- create-template.go
+++ create-template.go
@@ -1,59 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-	"time"
+	"github.com/wyster/go-sendgrid/template"
 )
 
 func main() {
-	var SendgridApiToken = os.Getenv("SENDGRID_TOKEN")
-	fmt.Println(SendgridApiToken)
-
-	req, err := http.NewRequest(
-		"POST",
-		"https://api.sendgrid.com/v3/templates",
-		strings.NewReader(`
-			{
-				"name":"test",
-				"generation":"dynamic"
-			}
-		`),
-	)
-	if err != nil {
-		log.Fatal("Error reading request. ", err)
-	}
-
-	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	req.Header.Set("Authorization", "Bearer "+SendgridApiToken)
-
-	// Set client timeout
-	client := &http.Client{Timeout: time.Second * 10}
-
-	fmt.Println(req.Header)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Error reading response. ", err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
-
-	var responseData struct {
-		Id         string `json:"id"`
-		Name       string `json:"name"`
-		Generation string `json:"generation"`
-		UpdatedAt  string `json:"updated_at"`
-	}
-	err = json.NewDecoder(resp.Body).Decode(&responseData)
-	if err != nil {
-		log.Fatal("Error reading response. ", err)
-	}
+	responseData := template.Create()
 
 	fmt.Println(responseData.Id)
 	fmt.Printf("Response: %+v", responseData)
